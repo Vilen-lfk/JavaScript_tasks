d663789f0b1acd9cbf5df36028ce18637e50d9b8
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-10-24 11:54:15 +0300

    18 задача

--- code.1.js
+++ code.1.js
@@ -6,6 +6,30 @@ alert(num);
 let a = 10;
 alert(a);
 a = 20;
-alert(a);*/
+alert(a);
 let b = 1 + 2 + 3;
-alert(b)
+alert(b)*/
+//18#1
+let a1 = 10;
+let b1 = 2;
+alert(a1 + b1);
+alert(a1 - b1);
+alert(a1 * b1);
+alert(a1 / b1);
+//18#2
+let c = 10;
+let d = 5;
+let result = c + d;
+alert(result);
+//18#3
+let a2 = 1;
+let b2 = 2;
+let c2 = 3;
+alert(a2 + b2 + c2)
+//18#4
+let a3 = 10;
+let b3 = 5;
+let c3 = a3 - b3;
+let d3 = 7;
+let result1 = c3 + d3;
+alert(result1);
