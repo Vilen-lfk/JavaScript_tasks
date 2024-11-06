fa2660cacf08e60d769af9c01bf888d5ce560936
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-06 09:04:14 +0300

    101 задача

---
+++ code.2.js
@@ -0,0 +1,29 @@
+//99
+let a = '99 задача';
+console.log(a);
+let test = {x: 1, y: 2, z: 3};
+console.log(test);
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1.x);
+let test3 = [1, 2, 3];
+let test2 = 1;
+console.log(test3[test2]);
+//100
+let a1 = '100 задача';
+console.log(a1);
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr1[0] = 'a';
+console.log(arr2);
+
+let arr3 = [1, 2, 3];
+let arr4 = arr3;
+arr3[0] = 'a';
+arr4[0] = 'b';
+console.log(arr4);
+//101
+let a2 = '101 задача';
+console.log(a2);
+const arr = ['a', 'b', 'c'];
+arr[1] = '!';
+console.log(arr);
