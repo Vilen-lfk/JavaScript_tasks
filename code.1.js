ee6ee82ba1dcc0ca3838b7c2a7a6bd90d8cda460
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-06 08:24:35 +0300

    93 задача

--- code.1.js
+++ code.1.js
@@ -358,3 +358,13 @@ let obj6 = {x: 1, y: 2, z: 3};
 console.log(obj6['x']);
 let key = 'x';
 console.log(obj6[key]);
+//93
+let key1 = 'x';
+let key2 = 'y';
+let key3 = 'z';
+let obj1 = {
+	[key1]: 1,
+	[key2]: 2,
+	[key3]: 3
+};
+console.log(obj1);
