0f9279245b6dd66f42a235b535f52c898c7cd762
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-07 08:26:37 +0300

    103 задача

--- code.2.js
+++ code.2.js
@@ -33,3 +33,20 @@ console.log(a3);
 const arr5 = [1, 2, 3, 4, 5];
 const res = arr5[1] + arr5[2];
 console.log(res);
+//103
+let a4 = '103 задача';
+console.log(a4);
+let obj = {x: 1, y: 2, z: 3};
+console.log(obj.x);
+
+let ob1 = {x: 1, y: 2, z: 3};
+let key = 'y';
+console.log(ob1[key]);
+
+let obj2 = {x: 1, y: 2, z: 3};
+let sum = obj2.x + obj2.y + obj2.z;
+console.log(sum);
+
+let obj3 = {x: 1, y: 2, z: 3};
+let size = Object.keys(obj3).length
+console.log(size);
