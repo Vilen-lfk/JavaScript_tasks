3ea56f8f533edc7df8a4f9fe4daaaa94fe5a9815
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-12 08:34:52 +0300

    127 задача

--- code.3.js
+++ code.3.js
@@ -5,3 +5,32 @@ if(tr == 10)
 {
     console.log('yes');
 }
+//125
+let a25 = '125 задача';
+console.log(a25);
+if (tr > 0) 
+	console.log('+++');
+else 
+	console.log('---');
+
+if (tr > 0) console.log('yes');
+
+/*Не смотря на то, что JavaScript допускает показанные выше сокращения с фигурными скобками,
+ я в общем-то не рекомендую так делать, так как это рассадник трудноуловимых ошибок*/
+
+//127
+let a27 = '127 задача';
+console.log(a27);
+const day = 30;
+if (day >= 1 && day <= 10) {
+    console.log('в первую');
+}
+else if (day >= 10 && day <= 20){
+    console.log('во вторую');
+}
+else if (day >= 20 && day <= 31){
+    console.log('в третью');
+}
+else {
+    console.log('неверное значение');
+}
