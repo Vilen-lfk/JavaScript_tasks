a4dcb7b7dcee2c369fb8a5c8c3b294f22bff1cbb
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-14 21:22:11 +0300

    146 задача

--- code.4.js
+++ code.4.js
@@ -5,3 +5,27 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 for (let elem of arr) {
 	console.log(elem);
 }
+//145
+let a45 = '145 задача';
+console.log(a45);
+let obj = {x: 1, y: 2, z: 3};
+for(let key in obj){
+    console.log(key);
+    console.log(obj[key]);
+}
+//146
+let a46 = '146 задача';
+console.log(a46);
+let i = 11;
+while(i <= 22){
+    console.log(i);
+    i++;
+}
+let num = 1;
+let iter = 0;
+while(num <= 100){
+    num = num * 3;
+    iter++;
+}
+console.log('result: ', num);
+console.log('Кол-во итeраций:', iter);
