5d2e666ea64494f14d7f2486b1a93e6f42d66a66
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-02-20 08:37:35 +0300

    200 задание

--- code.7.js
+++ code.7.js
@@ -1,3 +1,6 @@
+//199
+let one = '199 задание';
+console.log(one);
 let num = 12345;
 let arr = String(num).split('');
 
@@ -7,3 +10,23 @@ for (let digit of arr) {
 }
 
 console.log(prod);
+
+//200
+let two = '200 задание';
+console.log(two);
+function name(){
+	console.log('vilen');
+}
+name();
+ let tr ='-------------';
+ console.log(tr);
+ function summ() {
+	let res = 0;
+	for (let i = 1; i < 100; i++){
+		res += i;
+	}
+	console.log(res);
+ }
+ summ();
+
+ 
